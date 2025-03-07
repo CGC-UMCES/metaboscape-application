4cff5b1812477d1cfc03e8a03cc0fc8865088bf1
Mike O'Brien <obrien@umces.edu>
2025-03-07 17:38:35 -0500

    complete audit of CBP polygons and points. Closes #4.

--- R/CBP cell audit/cbp_cell_audit.R
+++ R/CBP cell audit/cbp_cell_audit.R
@@ -1,3 +1,6 @@
+## Audit run to compare CBP cells to model points
+## Completed 2025-03-07
+
 library(tidync)
 library(sf)
 library(dplyr)
@@ -13,6 +16,9 @@ model_points <- tidync("R/big data/whiteperch_95_96.nc") |>
 
 model_cells <- st_read("R/Chesapeake_Bay_Water_Quality_Modeling_cells.geojson")
 
+# Shift latitude down by 0.00225 to match model points
+st_geometry(model_cells) <- st_geometry(model_cells) - c(0, 0.00225)
+st_crs(model_cells) <- 4326
 
 cells_points_joined <- sf::st_join(
   model_cells,
@@ -20,13 +26,17 @@ cells_points_joined <- sf::st_join(
 ) |>
   mutate(cell = is.na(Cell_ID))
 
-the_map <- mapview(
+mapview(
   cells_points_joined,
   zcol = "cell"
 ) +
   mapview(model_points, cex = 2)
 
-the_map |>
-  mapshot("cbp_polygon_audit.html",
-    selfcontained = TRUE
-  )
+# CELLID is the cell ID from the CBP model polygons
+# Cell_ID is the cell ID from the model points
+key <- cells_points_joined |>
+  data.frame() |>
+  select(CELLID, Cell_ID) |>
+  distinct()
+
+write.csv(key, "R/cell_id_key.csv", row.names = FALSE)
