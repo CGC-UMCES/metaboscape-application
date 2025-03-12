c81c7967551ddb54cd9ba3cfcb4cb06d5adcfb59
Mike O'Brien <obrien@umces.edu>
2025-03-12 18:42:03 -0400

    Create working application. Closes #2, Closes #3, Closes #6.

--- R/app.R
+++ R/app.R
@@ -1,20 +1,41 @@
-## Dummy change for demonstration purposes
-
 # Shiny reference: https://mastering-shiny.org/index.html
 library(shiny)
 library(bslib)
 library(mapgl)
 library(tidync)
+library(dplyr)
 
 options(shiny.host = "0.0.0.0")
 options(shiny.port = 20688)
 
+# Metaboscape model output
 wp <- tidync("R/big data/whiteperch_95_96.nc")
 
+# Cell ID key of the CBP model domain. See R/CBP cell audit/cbp_cells.R
 domain <- sf::st_read(
-  "/home/R/Chesapeake_Bay_Water_Quality_Modeling_cells.geojson"
+  "/home/R/working data/model_cells.geojson"
 )
 
+#' @param depth_ft The depth in feet: 5-95, a multiple of 5
+#' @param date The date in the format "YYYY-MM-DD"
+cut_ncdf <- function(depth_ft, date) {
+  layer <- depth_ft / 5
+  date <- paste0(date, "-00")
+
+  domain |>
+    inner_join(
+      wp |>
+        tidync::hyper_filter(
+          Time = Time == date,
+          Layer_N = index == layer
+        ) |>
+        tidync::hyper_tibble() |>
+        dplyr::filter(nwcbox != -9999),
+      by = join_by(cell == nwcbox)
+    )
+}
+
+### The app ###
 ui <- page_sidebar(
   title = "The Chesapeake Metaboscape",
   sidebar = sidebar(
@@ -22,12 +43,28 @@ ui <- page_sidebar(
       selectInput(
         "select",
         "Select data",
-        choices = list("Depth" = 1, "IGR" = 2, "MR" = 3, "RM" = 4),
-        selected = 1
+        choices = list(
+          "IGR" = "IGR", "MF" = "MF", "RM" = "RM",
+          "Temperature (C)" = "T", "Salinity (ppt)" = "S",
+          "Dissolved Oxygen (mg/L)" = "DO"
+        ),
+        selected = "IGR"
+      ),
+      sliderInput(
+        "layer",
+        "Select depth (ft)",
+        min = 5,
+        max = 95,
+        step = 5,
+        value = 5
+      ),
+      dateInput(
+        "date",
+        "Select date",
+        min = "1995-01-01",
+        max = "1996-12-31",
+        value = "1995-01-01"
       )
-    ),
-    card(
-      dateInput("date", "Select date", value = "1995-01-01")
     )
   ),
   card(
@@ -37,6 +74,10 @@ ui <- page_sidebar(
 )
 
 server <- function(input, output, session) {
+  selected_data <- reactive({
+    cut_ncdf(input$layer, input$date)
+  })
+
   output$map <- renderMaplibre({
     maplibre(style = carto_style("positron")) |>
       fit_bounds(
@@ -44,16 +85,16 @@ server <- function(input, output, session) {
       ) |>
       add_fill_layer(
         id = "domain",
-        source = domain,
+        source = selected_data(),
         fill_color = interpolate(
-          column = "DEPTH",
-          values = range(domain$DEPTH, na.rm = TRUE),
+          column = input$select,
+          values = range(selected_data()[[input$select]], na.rm = TRUE),
           stops = c("blue", "red"),
           na_color = "lightgrey"
         ),
         fill_opacity = 0.5,
         fill_outline_color = "rgba(0, 0, 0, 0)",
-        tooltip = "DEPTH"
+        tooltip = input$select
       )
   })
 }
